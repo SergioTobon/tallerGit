2c69a26e61df1aacf1177d32c62fc240813e9335
SergioTobon <sestebantmontoya@gmail.com>
2025-02-26 21:03:46 -0500

    Se crea la funcionalidad sumar dos numeros

---
+++ createSuma/src/createsuma/CreateSuma.java
@@ -0,0 +1,11 @@
+
+package createsuma;
+
+public class CreateSuma {
+
+    public static void main(String[] args) {
+        int resultadoSuma = Sumar.sumar(9, 11);
+        System.out.println(resultadoSuma);
+    }
+    
+}
